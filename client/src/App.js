8c3c667f5edf62310bffa5566d33b135d9c26c43
Aditi R <aditi.seattle@gmail.com>
2020-03-29 15:15:17 -0700

    Add Login and Register pages. Also add routes for these pages.

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Footer from './components/layout/Footer';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Register from './components/auth/register';
+import Login from './components/auth/login';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <div className="App">
         <Navbar/>
         <Route exact path="/" component= {Landing}></Route>
+        <Route exact path="/register" component={Register} ></Route>
+        <Route exact path="/login" component={Login} ></Route>
         <Footer/>
       </div>
     </Router>
